45e67a3726012075b623b05d8e16352f38261bcc
zhangxiaojian19926 <505314607@qq.com>
2023-05-13 13:00:51 +0800

    第四次 扩展实现斐波那契数列

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,12 +1,44 @@
 use std::io;
 
 // 递归实现斐波那契数列
+//方式1 以match方式实现
+// fn fb(x:u32)->u32{
+//     match x {
+//         0 => 1,
+//         1 => 1,
+//         _ => fb(x - 1) + fb(x - 2)
+//     }
+// }
+
+// 方式2 普通递归实现
+// fn fb(x:u32)->u32 {
+//     return if x < 2 { 1 } else {fb(x -1) + fb(x - 2)};
+// }
+
+//方式3：不通过递归实现
 fn fb(x:u32)->u32{
-    match x {
-        0 => 1,
-        1 => 1,
-        _ => fb(x - 1) + fb(x - 2)
+    // f2 = f1 + f0
+    // f3 = f2 + f1
+    // f4 = f3 + f2 = f2 + f1 + f2
+
+    let mut p1 = 0;
+    let mut p2 = 1;
+    let mut now = 1;
+
+    // while x > 0 {
+    //     now = p1 + p2;
+    //     p1 = p2;
+    //     p2 = now;
+    //     x -= 1;
+    // }
+
+    for _i in 0..x {
+        now = p1 + p2;
+        p1 = p2;
+        p2 = now;
     }
+
+    return now;
 }
 
 fn main() {
